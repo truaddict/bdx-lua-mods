2532bb060bf7abad2e79a3ed03264bc38175517e
Truaddict <480471+truaddict@users.noreply.github.com>
2020-04-22 14:24:30 +0300

    code optimization

--- localchat/localchat.lua
+++ localchat/localchat.lua
@@ -1,22 +1,35 @@
 json = require "json"
 
+
+DISTANCE = 100		-- local chat radius in blocks
+ADMINTAG = "admin"  -- use in-game command /tag <player> add <ADMINTAG>
+OP_PREFIX = "[§cA§r]"
+
+
+function rawtext(content)
+	return json.encode({rawtext = {{text = content}} })
+end
+
+function opPrefix(playername)
+	if(isOP(playername)) then
+		return OP_PREFIX .. playername
+	else
+		return playername
+	end
+end
+
+
 Listen("onChat",function(name,text)
 
     if(text:sub(1,1) == "!")
     then
     	--global chat--
-    	chatStr1 = "§6Ⓖ§r " .. name .. ": " .. text:sub(2)
-    	chatStr2 = json.encode({rawtext = {{text = chatStr1}} })
-		runCmd("tellraw @a " .. chatStr2)
+		runCmd("tellraw @a " .. rawtext("§6Ⓖ§r " .. opPrefix(name) .. ": " .. text:sub(2)))
     else
 		--local chat--
-		chatStr1 = "§3Ⓛ§r " .. name .. ": " .. text
-    	chatStr2 = json.encode({rawtext = {{text = chatStr1}} })
-		runCmd("execute " .. name .. " ~ ~ ~ tellraw @a[r=100] " .. chatStr2)
+		runCmd(string.format("execute %s ~ ~ ~ tellraw @a[r=%i] %s", name, DISTANCE, rawtext("§3Ⓛ§r " .. opPrefix(name) .. ": " .. text)))
 		--send to admins--
-		spyStr1 = "§7Ⓛ <" .. name .. "> " .. text
-		spyStr2 = json.encode({rawtext = {{text = spyStr1}} })
-		runCmd("execute " .. name .. " ~ ~ ~ tellraw @a[rm=101,tag=admin] " .. spyStr2)
+		runCmd(string.format("execute %s ~ ~ ~ tellraw @a[rm=%i,tag=%s] %s", name, DISTANCE + 1, ADMINTAG, rawtext("§7Ⓛ <" .. opPrefix(name) .. "> " .. text)))
 	end
 	
 	return -1
