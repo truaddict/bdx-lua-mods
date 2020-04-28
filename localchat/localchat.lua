dfe68de7abbbd958b43622daf70038d2d0e93c46
truaddict <480471+truaddict@users.noreply.github.com>
2020-04-29 00:04:51 +0300

    fix for names with spaces

--- localchat/localchat.lua
+++ localchat/localchat.lua
@@ -29,9 +29,9 @@ Listen("onChat", function(name,text)
 		runCmd("tellraw @a " .. rawtext("§6Ⓖ§r " .. opPrefix(name) .. ": " .. text:sub(2)))
     else
 		--local chat
-		runCmd(string.format("execute %s ~ ~ ~ tellraw @a[r=%i] %s", name, DISTANCE, rawtext("§3Ⓛ§r " .. opPrefix(name) .. ": " .. text)))
+		runCmd(string.format("execute \"%s\" ~ ~ ~ tellraw @a[r=%i] %s", name, DISTANCE, rawtext("§3Ⓛ§r " .. opPrefix(name) .. ": " .. text)))
 		--send to ops (doesn't work if op is in different dimension)
-		runCmd(string.format("execute %s ~ ~ ~ tellraw @a[rm=%i,tag=%s] %s", name, DISTANCE + 1, OP_TAG, rawtext("§7Ⓛ <" .. opPrefix(name) .. "> " .. text)))
+		runCmd(string.format("execute \"%s\" ~ ~ ~ tellraw @a[rm=%i,tag=%s] %s", name, DISTANCE + 1, OP_TAG, rawtext("§7Ⓛ <" .. opPrefix(name) .. "> " .. text)))
 	end
 	
 	return -1
