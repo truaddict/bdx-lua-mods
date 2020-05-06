979b83b25da9a522441a88621c2e3c9ab55d9f72
truaddict <480471+truaddict@users.noreply.github.com>
2020-05-06 21:31:52 +0300

    Update localchat.lua

--- localchat/localchat.lua
+++ localchat/localchat.lua
@@ -7,18 +7,37 @@ local DISTANCE = 100		-- local chat radius in blocks
 local OP_TAG = "admin"  -- use in-game command /tag <player> add <OP_TAG>
 local OP_PREFIX = "[§cA§r]"
 
+local cnames = {
+	["Steve"] = "§Hacker§r",
+	["Mike"] = "§fCoolMiner§r"
+}
+
+local prefixes = {
+	["Steve"] = "[§fOP§r]",
+	["Mike"] = "[§fmember§r]"
+}
+
 
 function rawtext(content)
 	return json.encode({rawtext = {{text = content}} })
 end
 
-function opPrefix(playername)
-	if(isOP(playername)) then
-		return OP_PREFIX .. playername
+function get_cname(playername)
+	local pref = ""
+	local cname = playername
+	if(prefixes[playername] ~= nil) then
+		pref = prefixes[playername]
 	else
-		return playername
+		if(isOP(playername)) then
+			pref = OP_PREFIX
+		end
 	end
+	if(cnames[playername] ~= nil) then
+		cname = cnames[playername]
+	end
+	return pref .. cname
 end
+
 
 
 Listen("onChat", function(name,text)
@@ -29,9 +48,9 @@ Listen("onChat", function(name,text)
 		runCmd("tellraw @a " .. rawtext("§6Ⓖ§r " .. opPrefix(name) .. ": " .. text:sub(2)))
     else
 		--local chat
-		runCmd(string.format("execute \"%s\" ~ ~ ~ tellraw @a[r=%i] %s", name, DISTANCE, rawtext("§3Ⓛ§r " .. opPrefix(name) .. ": " .. text)))
+		runCmd(string.format("execute \"%s\" ~ ~ ~ tellraw @a[r=%i] %s", name, DISTANCE, rawtext("§3Ⓛ§r " .. get_cname(name) .. ": " .. text)))
 		--send to ops (doesn't work if op is in different dimension)
-		runCmd(string.format("execute \"%s\" ~ ~ ~ tellraw @a[rm=%i,tag=%s] %s", name, DISTANCE + 1, OP_TAG, rawtext("§7Ⓛ <" .. opPrefix(name) .. "> " .. text)))
+		runCmd(string.format("execute \"%s\" ~ ~ ~ tellraw @a[rm=%i,tag=%s] %s", name, DISTANCE + 1, OP_TAG, rawtext("§7Ⓛ <" .. get_cname(name) .. "> " .. text)))
 	end
 	
 	return -1
